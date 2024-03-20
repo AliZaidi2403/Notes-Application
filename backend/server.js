1388c5f200501ce0a2283991013e5a44889270bd
Amaan <amaanzaidi462@gmail.com>
2024-03-20 23:33:55 +0530

    First commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const notes = require("./data/notes");
+const app = express();
+dotenv.config();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running at port 5000");
+});
+app.get("/api/notes", (req, res) => {
+  res.send(notes);
+});
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((e) => e._id === req.params.id);
+  res.send(note);
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server is listenning at port 5000");
+});
